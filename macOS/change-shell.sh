5c2aa803f839e09382b53a4e7626fce0eb881cc8
Gianluca Tranchedone <g.tranchedone@gmail.com>
2016-11-01 13:05:38 +0000

    Fix issue when copying dotfiles

--- macOS/change-shell.sh
+++ macOS/change-shell.sh
@@ -10,7 +10,7 @@ echo "$(tput setaf 6)==>$(tput sgr0) Install oh-my-zsh..."
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 # Customize shell
 echo "$(tput setaf 6)==>$(tput sgr0) Install dotfiles and oh-my-zsh cutomizations..."
-cp $INSTALL_SCRIPT_DIRECTORY/shell/zsh/oh-my-zsh-custom/* ~/.oh-my-zsh/custom
-cp $INSTALL_SCRIPT_DIRECTORY/shell/zsh/dotfiles/* ~/
+cp -a $INSTALL_SCRIPT_DIRECTORY/shell/zsh/oh-my-zsh-custom/. ~/.oh-my-zsh/custom
+cp -a $INSTALL_SCRIPT_DIRECTORY/shell/zsh/dotfiles/. ~/
 # Reload
 source ~/.zshrc
